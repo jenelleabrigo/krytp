0764964250146d1d61871bed4af600e4914a471f
Jenelle Loise Abrigo <jenelleabrigo@gmail.com>
2022-09-17 22:47:21 +0800

    krypt project finish

---
+++ src/utils/constants.js
@@ -0,0 +1,4 @@
+import abi from "./Transactions.json";
+
+export const contractABI = abi.abi;
+export const contractAddress = "0x3A72585610828D4d18e2a445926A54e5C539D530";
---
+++ src/utils/truncateAddress.js
@@ -0,0 +1,1 @@
+export const truncateAddress = (address) => `${address.slice(0, 5)}...${address.slice(address.length - 4)}`;
